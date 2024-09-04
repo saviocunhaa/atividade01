afc87cf7cd0983f9afa1184946ee089cab7bc831
saviocunhaa <saviocunha.01@hotmail.com>
2024-09-04 14:34:07 -0300

    soma de numeros

---
+++ ati.py
@@ -0,0 +1,9 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+
+val1 = int(input("digite o 1º valor"))
+val2 = int(input("digite o 2º valor"))
+
+soma = val1 + val2
+
+print(f"a soma do valor é = {soma}")
+
